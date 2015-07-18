581cafa3ce1fb1d06e29094f0ad06230cc38b8c0
Simon Forsberg <zomis2k@hotmail.com>
2015-07-18 18:45:53 +0200

    added InviteResponse ToString

--- src/main/java/net/zomis/server/messages/both/InviteResponse.groovy
+++ src/main/java/net/zomis/server/messages/both/InviteResponse.groovy
@@ -1,10 +1,12 @@
 package net.zomis.server.messages.both
 
+import groovy.transform.ToString
 import net.zomis.server.messages.FourChar
 import net.zomis.server.messages.Message
 
 @FourChar(value = 'INVR', incomingStr = {mess -> new InviteResponse(inviteId: mess[1] as int, accepted: mess[2] == '1') },
     outgoingStr = { "$inviteId $responder ${accepted ? '1' : '0'}" })
+@ToString
 class InviteResponse implements Message {
 
     boolean accepted
